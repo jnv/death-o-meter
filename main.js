5fbe575860f76c0fdbe5e667ef023fe7e7f5129a
Jan Vlnas <git@jan.vlnas.cz>
2017-01-01 19:32:15 +0100

    Import what we have so far

---
+++ main.js
@@ -0,0 +1,241 @@
+'use strict'
+
+var tooltip;
+
+var CIRCLE_R = 2.5
+var CHART_MARGIN = {top: CIRCLE_R, right: CIRCLE_R, bottom: 30, left: CIRCLE_R}
+var TOTALS
+
+function prop (name) {
+  return function (obj) {
+    return obj[name]
+  }
+}
+
+function parseRow (d) {
+  return {
+    category: d.category,
+    name: d.name,
+    cause: d.cause,
+    date: new Date(d.death),
+  }
+}
+
+var MONTH_NAMES = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+]
+
+var CATEGORIES = {
+  actors: true,
+  singers: true,
+  writers: true,
+}
+
+function monthName (i) {
+  return MONTH_NAMES[i]
+}
+
+var formatDate = d3.timeFormat("%Y-%m-%d")
+var formatMonth = d3.timeFormat("%Y-%m")
+
+function yearNest () {
+  return d3.nest()
+    .key(function(d) { return d.date.getFullYear() }).sortKeys(d3.ascending)
+}
+
+function groupByDay () {
+  return d3.nest()
+    .key(function(d) { return formatDate(d.date) })
+    .sortKeys(d3.ascending)
+    .sortValues(function (a, b) { return d3.ascending(a.category, b.category) })
+}
+
+function calculateTotals (data) {
+  return yearNest()
+    .key(function(d) { return d.category })
+    .rollup(function(leaves) { return leaves.length })
+    .map(data)
+}
+
+function updateCounts (data, target) {
+  var prefix = '.js-count-' + target + '-'
+
+  var total = 0
+  var others = 0
+  data.each(function(count, cat) {
+    d3.selectAll(prefix + cat).text(count)
+    if (CATEGORIES[cat]) {
+      total += count
+    } else {
+      others += count
+    }
+  })
+  d3.selectAll(prefix + 'total').text(total)
+  d3.selectAll(prefix + 'others').text(others)
+}
+
+function groupByMonth () {
+  return d3.nest()
+    .key(function (d) { return d.date.getMonth() })
+    .sortKeys(d3.ascending)
+    // .sortValues(function (a, b) { return d3.ascending(a.date, b.date) })
+    .sortValues(function (a, b) { return d3.ascending(a.category, b.category) })
+}
+
+function dailyChart (data, year, svg) {
+  var circleR = CIRCLE_R
+  var margin = CHART_MARGIN
+  var width = +svg.attr('width') - margin.left - margin.right
+  var height = +svg.attr('height') - margin.top - margin.bottom
+  var xScale = d3.scaleTime()
+    .domain([new Date(year, 0, 1), new Date(year, 11, 30)])
+    .range([0, width])
+
+  var xAxis = d3.axisBottom(xScale)
+                .ticks(d3.utcMonth.every(1))
+                .tickFormat(d3.timeFormat('%B'))
+  // var x = d3.scaleBand().rangeRound([0, width]).padding(0.1)
+
+  var yScale = d3.scaleLinear()
+          .range([height, 0])
+          .domain([0, 25]) // XXX assumes there's max n points per day
+
+  var g = svg.append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+  g.append('g')
+      .attr('class', 'axis axis-x')
+      .attr('transform', 'translate(0,' + (height + 6) + ')')
+      .call(xAxis)
+
+  var days = g.selectAll('.day')
+    .data(data)
+    .enter().append('g')
+      .attr('class', 'day')
+      .attr('data-date', function (d) { return d.key })
+
+  days.selectAll('circle.person')
+    .data(function (d, i) { return d.values })
+    .enter().append('circle')
+    .attr('data-name', prop('name'))
+    .attr('data-category', prop('category'))
+    .attr('data-cause', prop('cause'))
+    .attr('class', 'person')
+    .attr('r', circleR)
+    .attr('cx', function (d) {
+      return xScale(d.date)
+    })
+    .attr('cy', function (d, i) {
+      return yScale(i)
+    })
+    .on('mouseover', function(d) {
+      tooltip.attr('class', 'active')
+      tooltip.html(d.name + '<br/>' + formatDate(d.date))
+        .style('left', (d3.event.pageX) + 'px')
+        .style('top', (d3.event.pageY - 28) + 'px');
+     })
+    .on('mouseout', function(d) {
+      tooltip.attr('class', '')
+    });
+}
+
+function monthlyChart (data, svg) {
+  var circleR = CIRCLE_R
+  var margin = CHART_MARGIN
+  var width = +svg.attr('width') - margin.left - margin.right
+  var height = +svg.attr('height') - margin.top - margin.bottom
+  var xScale = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.1)
+    .domain(data.map(prop('key')))
+
+  var dotsPerMonth = Math.floor(xScale.bandwidth() / (circleR * 2))
+
+  var xAxis = d3.axisBottom(xScale)
+                // .ticks(d3.utcMonth.every(1))
+                .tickFormat(monthName)
+  // var x = d3.scaleBand().rangeRound([0, width]).padding(0.1)
+
+  var yScale = d3.scaleLinear()
+          .range([height, 0])
+          .domain([0, 25]) // XXX assumes there's max n points per day
+
+
+  svg.select('g.root').remove()
+
+  var g = svg.append('g')
+      .attr('class', 'root')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+  g.append('g')
+      .attr('class', 'axis axis-x')
+      .attr('transform', 'translate(0,' + (height + 6) + ')')
+      .call(xAxis)
+
+  var days = g.selectAll('.month')
+    .data(data)
+    .enter().append('g')
+      .attr('class', 'month')
+      .attr('data-month', function (d) { return d.key })
+      .attr('transform', 'translate(' + circleR * 2 + ',0)')
+
+  days.selectAll('circle.person')
+    .data(function (d) { return d.values })
+    .enter().append('circle')
+    .attr('data-name', prop('name'))
+    .attr('data-category', prop('category'))
+    .attr('data-cause', prop('cause'))
+    .attr('class', 'person')
+    .attr('r', circleR)
+    .attr('cx', function (d, i) {
+      return xScale(d.date.getMonth()) + (i % dotsPerMonth) * (circleR * 2)
+    })
+    .attr('cy', function (d, i) {
+      return yScale(Math.floor(i / dotsPerMonth))
+    })
+    .on('mouseover', function(d) {
+      tooltip.attr('class', 'active')
+      tooltip.html(d.name + '<br/>' + formatDate(d.date))
+        .style('left', (d3.event.pageX) + 'px')
+        .style('top', (d3.event.pageY - 28) + 'px');
+     })
+    .on('mouseout', function(d) {
+      tooltip.attr('class', '')
+    });
+}
+
+d3.json('data.json', function (error, rawData) {
+  if (error) {
+    throw error
+  }
+
+  tooltip = d3.select('body').append('div')
+    .attr('id', 'chart-tooltip')
+
+  var data = rawData.map(parseRow)
+
+  TOTALS = calculateTotals(data)
+  updateCounts(TOTALS.get(2016), 2016)
+
+  var years = yearNest().object(data)
+  var yearByDay = groupByDay().entries(years['2016'])
+  var yearByMonth = groupByMonth().entries(years['2016'])
+
+  dailyChart(yearByDay, 2016, d3.select('#chart-daily'))
+  monthlyChart(yearByMonth, d3.select('#chart-compare-base'))
+
+  function setComparison(year) {
+    d3.selectAll('.js-cmp-year').text(year)
+    updateCounts(TOTALS.get(year), 'cmp')
+    monthlyChart(groupByMonth().entries(years[year]), d3.select('#chart-compare-cmp'))
+  }
+
+  setComparison(2015)
+
+  var $yearSelect = d3.select('#js-cmp-year-select')
+
+  $yearSelect.on('change', function(e) {
+    var value = $yearSelect.node().value
+    setComparison(value)
+  })
+})
